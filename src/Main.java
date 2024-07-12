52728397f2bb5ca0f36dd4a0927ce76b0f863980
= <=>
2024-07-12 18:36:04 +0300

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Word: ");
+        String word=input.nextLine();
+        System.out.println(reverse(word));
+    }
 
+    static String reverse(String word){
+        int length=word.length();
+        String reverse="";
+        for(int i=0;i<length;i++){
+            String letter= String.valueOf(word.charAt(length-1-i));
+            reverse=reverse.concat(letter);
+        }
+        return reverse;
     }
 }
